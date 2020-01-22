7441c08d5f812600a47c96c4eb320e5757bb32a3
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 15:50:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   puts %w(string)
-  array = %w string
-  new_array = array.collect {|i| i.reverse}
-  puts new_array.join(' ')
+  # array = %w string
+  # new_array = array.collect {|i| i.reverse}
+  # puts new_array.join(' ')
 end
